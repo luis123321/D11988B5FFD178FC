298d740a0daa4a6b65aaec4db0323f14777441da
Luis123321 <laetutoriales@gmail.com>
2015-06-16 00:22:45 -0400

    Mas cambios

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php 
     include 'php/conexion.php';
-    $sql = ("SELECT id,title,content FROM frontpagecontent WHERE id='1'");
-    $query = mysqli_query($dbconnect,$sql);
-    if($query){
-    $row = mysqli_fetch_row($query);
-        $id = $row[0];
-        $title = $row[1];
-        $content = $row[2];
-    };
+    include 'php/reader.php';
 
  ?>
 
